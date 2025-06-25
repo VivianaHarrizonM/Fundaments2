3923c406ee304a4adf073f03e9e8c5c9c0c17fe4
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-06-24 19:33:51 -0600

    Changes in peliculas

--- src/peliculas/datos/AccesoDatosImpl.java
+++ src/peliculas/datos/AccesoDatosImpl.java
@@ -32,6 +32,7 @@ public class AccesoDatosImpl implements IAccesoDatos{
                 linea = entrada.readLine();
             }
             entrada.close();
+            
         } catch (FileNotFoundException ex) {
            ex.printStackTrace();
            throw new LecturaDatosEx("Excepción al listar peliculas" + ex.getMessage());
@@ -45,20 +46,68 @@ public class AccesoDatosImpl implements IAccesoDatos{
     @Override
     public void escribir(Pelicula pelicula, String nombreRecurso, boolean anexar) throws EscrituraDatosEx {
         var archivo = new File(nombreRecurso);
+        try {
+            var salida = new PrintWriter(new FileWriter(archivo, anexar));
+            salida.println(pelicula.toString());
+            salida.close();
+            System.out.println("Se ha escrito información al archivo: " + pelicula);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new EscrituraDatosEx("Excepción al escribir peliculas" + ex.getMessage());
+        }
     }
+    
 
     @Override
     public String buscar(String nombreRecurso, String buscar) throws LecturaDatosEx {
         var archivo = new File(nombreRecurso);
+        String resultado = null;
+        try {
+            var entrada = new BufferedReader(new FileReader(archivo));
+            String linea = null;
+            linea = entrada.readLine();
+            var indice = 1;
+            
+            while(linea != null){
+                if(buscar != null && buscar.equalsIgnoreCase(linea)){
+                    resultado = "Pelicula encontrada " +linea + "en elindice " + indice;
+                    break;
+                }
+                linea = entrada.readLine();
+                indice++;
+            }
+            entrada.close();
+        } catch (FileNotFoundException ex) {
+             ex.printStackTrace();
+            throw new LecturaDatosEx("Excepción al bucar la peliculas" + ex.getMessage());
+        } catch (IOException ex) {
+           ex.printStackTrace();
+            throw new LecturaDatosEx("Excepción al bucar la peliculas" + ex.getMessage());
+        }
+        return resultado;
     }
 
     @Override
     public void crear(String nombreRecurso) throws AccesoDatosEx {
+         var archivo = new File(nombreRecurso);
+         
+        try {
+            var salida = new PrintWriter(new FileWriter(archivo));
+            salida.close();
+            System.out.println("Se ha creado el archivo");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new AccesoDatosEx("Excepción al crear archivo" + ex.getMessage());
+        }
     }
 
     @Override
     public void borrar(String nombreRecurso) throws AccesoDatosEx {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         var archivo = new File(nombreRecurso);
+         if(archivo.exists()){
+             archivo.delete();
+         }
+         System.out.println("Seha eliminado el archivo");
     }
     
 }
--- src/peliculas/dominio/Pelicula.java
+++ src/peliculas/dominio/Pelicula.java
@@ -20,7 +20,11 @@ public class Pelicula {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
+
+    @Override
+    public String toString() {
+        return this.nombre;
+    }
     
     
 }
